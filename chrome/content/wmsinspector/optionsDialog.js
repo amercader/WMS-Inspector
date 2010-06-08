fed906734af3554c8d221c7faf43fb8e50f7398f
amercader <amercadero@gmail.com>
2010-06-08 17:34:56 +0100

    Add preference value for default WMS version (Issue #8)

--- chrome/content/wmsinspector/optionsDialog.js
+++ chrome/content/wmsinspector/optionsDialog.js
@@ -7,6 +7,17 @@ WMSInspector.OptionsDialog = {
         this.prefs = WMSInspector.Utils.getPrefs();
         
         //Set values from current preferences values
+        var versions = this.prefs.getCharPref("wmsversions").split("|");
+        var defVersion = this.prefs.getCharPref("wmsversion");
+
+        var menuList = document.getElementById("wiVersionMenu");
+        var item;
+
+        for (var i=0; i < versions.length; i++){
+            item = menuList.appendItem(versions[i],i);
+            if (versions[i] == defVersion)  menuList.selectedItem = item;
+        }
+
         document.getElementById("wiHideContextMenu").checked = this.prefs.getBoolPref("hidecontextmenu");
         document.getElementById("wiEditorPath").value = this.prefs.getCharPref("editor");
     },
@@ -25,6 +36,9 @@ WMSInspector.OptionsDialog = {
     },
 
     onAccept: function(){
+
+        //Default WMS version
+        this.prefs.setCharPref("wmsversion",document.getElementById("wiVersionMenu").selectedItem.getAttribute("label"));
 
         //Hide context menu
         this.prefs.setBoolPref("hidecontextmenu",(document.getElementById("wiHideContextMenu").checked === true));
